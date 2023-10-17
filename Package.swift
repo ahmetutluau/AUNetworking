ef9120219671ebb8a9bec55c2a683841ea7374f1
Ahmet Utlu <ahmet.utlu@basefy.com>
2023-10-17 16:47:11 +0200

    - test file deleted.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "AUNetworking",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AUNetworking",
             targets: ["AUNetworking"]),
@@ -15,14 +14,9 @@ let package = Package(
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.8.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AUNetworking",
             dependencies: ["Alamofire"],
-            path: "Sources"),
-        .testTarget(
-            name: "AUNetworkingTests",
-            dependencies: ["AUNetworking"]),
+            path: "Sources")
     ]
 )
--- Tests/AUNetworkingTests/AUNetworkingTests.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import AUNetworking
-
-final class AUNetworkingTests: XCTestCase {
-    
-}
