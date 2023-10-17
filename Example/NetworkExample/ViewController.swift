0180fec7e746676c075f8f58c1656fc211e3abb6
Ahmet Utlu <ahmet.utlu@basefy.com>
2023-10-17 17:10:24 +0200

    - NetworkExapmle file updated.

--- Example/NetworkExample/ViewController.swift
+++ Example/NetworkExample/ViewController.swift
@@ -12,10 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        APIConstants.baseURL = "https://api.themoviedb.org/3"
         getPopularMovies()
+        getDetail()
     }
 
+//    https://developer.themoviedb.org/reference/movie-popular-list
     func getPopularMovies() {
         Networking.request(router: MovieRouter.popular) { (response: PopularResponseModel?, errorString) in
 
@@ -30,7 +32,21 @@ class ViewController: UIViewController {
         }
         
     }
-
+    
+//    https://developer.themoviedb.org/reference/movie-details
+    func getDetail() {
+        Networking.request(router: MovieRouter.detail(646389)) { (response: DetailResponseModel?, errorString) in
+            
+            guard let response = response else { return }
+            
+            print("iiiiii......\(response)")
+            
+        } onFailure: { (errorString, errorType) in
+            guard let error = errorString else { return }
+            
+            print(error)
+        }
+    }
 
 }
 
